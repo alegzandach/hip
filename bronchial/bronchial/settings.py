c46cabd32ea530b7fe7c0f09c738699596f419ed
Rakesh Guha <guha.rakesh@gmail.com>
2015-06-07 20:23:02 -0400

    dynamically generate MEDIA_ROOT

--- bronchial/bronchial/settings.py
+++ bronchial/bronchial/settings.py
@@ -101,6 +101,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+ENV_PATH = os.path.abspath(os.path.dirname(__file__))
+MEDIA_ROOT = os.path.join(ENV_PATH, 'media/')
+
 STATIC_URL = '/static/'
 STATIC_URL = '/media/'
 
