f65eb9eaf8f4926bf2225f6e0640a725ac60e0d5
Rakesh Guha <guha.rakesh@gmail.com>
2015-06-07 20:25:16 -0400

    serve files in media root

--- bronchial/bronchial/urls.py
+++ bronchial/bronchial/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from rest_framework import routers
 from stent import views
+from django.conf import settings
 
 router = routers.DefaultRouter()
 router.register(r'airways', views.AirwayViewSet)
@@ -11,3 +12,9 @@ urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns.append(url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.MEDIA_ROOT,
+    }))
+    
