e23da22a2d73ccc63cc588a64954551c909e1fc1
Rakesh Guha <guha.rakesh@gmail.com>
2015-06-09 10:04:51 -0400

    api endpoints on /api

--- bronchial/bronchial/urls.py
+++ bronchial/bronchial/urls.py
@@ -8,8 +8,8 @@ router = routers.DefaultRouter()
 router.register(r'airways', views.AirwayViewSet)
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api/', include(router.urls)),
+    url(r'^api/api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', include(admin.site.urls)),
 ]
 
