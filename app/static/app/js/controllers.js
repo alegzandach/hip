67c4a6a42112bc0f95dd72858ec5b0089284bb82
Rakesh Guha <guha.rakesh@gmail.com>
2015-06-10 17:10:37 -0400

    build airway list controller

--- app/static/app/js/controllers.js
+++ app/static/app/js/controllers.js
@@ -1,3 +1,11 @@
 'use strict';
 
-/* Controllers */
+var stentApp = angular.module('stentApp', []);
+
+stentApp.controller('AirwayListCtrl', function($scope) {
+  $scope.airways= [
+    {'name': 'Airway1'},
+    {'name': 'Airway2'},
+    {'name': 'Airway3'}
+  ]
+});
