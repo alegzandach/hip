69e6d3336b0a962e34af28f787a7acbb44f32a8e
Rakesh Guha <guha.rakesh@gmail.com>
2015-06-11 17:50:27 -0400

    wire controller to backend

--- app/static/app/js/controllers.js
+++ app/static/app/js/controllers.js
@@ -2,10 +2,8 @@
 
 var stentApp = angular.module('stentApp', []);
 
-stentApp.controller('AirwayListCtrl', function($scope) {
-  $scope.airways= [
-    {'name': 'Airway1'},
-    {'name': 'Airway2'},
-    {'name': 'Airway3'}
-  ]
-});
+stentApp.controller('AirwayListCtrl', ['$scope', '$http', function($scope, $http) {
+  $http.get('api/airways').success(function(data) {
+    $scope.airways = data;
+  });
+}]);
